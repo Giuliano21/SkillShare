dfbaa02dc02a837b5ef2133c5a5543612f1475a1
Giuliano21 <capitelligiuliano21@gmail.com>
2026-08-03 19:31:10 +0200

    Aggiunta dell' hashing della password e del comparatore di password

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,7 +1,6 @@
-// Questo modello definisce la struttura dei dati per gli utenti nel database MongoDB
+// User.js definisce la struttura dei dati per gli utenti nel database MongoDB
 
 const mongoose = require('mongoose');
-// bcryptjs è una libreria per l'hashing delle password. Fornisce funzioni per hashare le password e confrontarle in modo sicuro.
 const bcrypt = require('bcryptjs');
 
 // definizione dello schema per il modello User
@@ -14,6 +13,12 @@ const userSchema = new mongoose.Schema({
     surname: {
         type: String,
         required: true,
+        trim: true,
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true,
         trim: true,
     },
     email: {
@@ -40,5 +45,18 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// Middleware che, prima di salvare una password, rende la password crittografata utilizzando bcrypt
+userSchema.pre('save' , async function(next){
+
+    if(!this.isModified('password')) return next();
+    // 10 è la lunghezza del salt
+    this.password = await bcrypt.hash(this.password , 10);
+    next();
+});
+
+// Metodo per verificare che la password inserita dall'utente corrisponda alla password hashata nel database
+userSchema.comparePassword() = async function(candidatePassword){
+    return await bcrypt.compare(candidatePassword, this.password);
+}
 
 module.exports = mongoose.model('User', userSchema);
