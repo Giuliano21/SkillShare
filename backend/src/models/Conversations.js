28baaaa73b670e355195ee3c4e92259c10019c21
Giuliano21 <capitelligiuliano21@gmail.com>
2026-08-17 14:41:00 +0200

    Aggiunta key unica per i partecipanti alla conversazione

--- backend/src/models/Conversations.js
+++ backend/src/models/Conversations.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
 const conversationSchema= new mongoose.Schema({
-    partecipants: [{
+    participants: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
-    }]
+    }],
+    // Campo per memorizzare una chiave unica basata sugli ID dei partecipanti
+    participantsKey: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    }
 },{timestamps: true} );
+
+// Indice per ottimizzare le query
+conversationSchema.index({ participants: 1 });
 
 module.exports = mongoose.model('Conversation' , conversationSchema);
