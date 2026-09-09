dcbe840fe99620cda691e6bb095a6f88bc38375c
Giuliano21 <capitelligiuliano21@gmail.com>
2026-09-09 15:43:52 +0200

    aggiunta dell hash per verify token

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,16 +1,20 @@
 /* authRoutes.js definisce le rotte per la registrazione, il login, il logout e il refresh del token di accesso degli utenti.
  Queste rotte vengono gestite dal controller authController.js, che contiene la logica per eseguire le operazioni di autenticazione. */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Importo authController per gestire le richieste di registrazione, login, logout e refresh del token di accesso
-const AuthController = require('../controllers/authController');
-const { authIpLimiter, authAccountLimiter, registerLimiter } = require('../middlewares/rateLimiters');
+const AuthController = require("../controllers/authController");
+const {
+  authIpLimiter,
+  authAccountLimiter,
+  registerLimiter,
+} = require("../middlewares/rateLimiters");
 
 // Rotte per la registrazione, login, logout e refresh del token di accesso degli utenti
-router.post('/register' , registerLimiter, AuthController.register);
-router.post('/login' , authIpLimiter, authAccountLimiter, AuthController.login);
-router.post('/logout' , AuthController.logout);
-router.post('/refresh' , AuthController.refresh);
+router.post("/register", registerLimiter, AuthController.register);
+router.post("/login", authIpLimiter, authAccountLimiter, AuthController.login);
+router.post("/logout", AuthController.logout);
+router.post("/refresh", AuthController.refresh);
 module.exports = router;
