a1496ca00bcdf4170321d178a82cc6a4ea4e184c
Giuliano21 <capitelligiuliano21@gmail.com>
2026-09-09 12:33:29 +0200

    aggiunta dei rate limiter nelle route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -6,10 +6,11 @@ const router = express.Router();
 
 // Importo authController per gestire le richieste di registrazione, login, logout e refresh del token di accesso
 const AuthController = require('../controllers/authController');
+const { authIpLimiter, authAccountLimiter, registerLimiter } = require('../middlewares/rateLimiters');
 
 // Rotte per la registrazione, login, logout e refresh del token di accesso degli utenti
-router.post('/register' , AuthController.register);
-router.post('/login' , AuthController.login);
+router.post('/register' , registerLimiter, AuthController.register);
+router.post('/login' , authIpLimiter, authAccountLimiter, AuthController.login);
 router.post('/logout' , AuthController.logout);
 router.post('/refresh' , AuthController.refresh);
 module.exports = router;
